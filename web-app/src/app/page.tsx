eaeabc92136a4b7ff0b1510a2d343c2614eda55e
zeeenku <enajjachi.zakariaa@gmail.com>
2025-02-10 22:17:58 +0100

    layout enhanced

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -6,44 +6,35 @@ import UiExamples from "@/components/ui-examples";
 
 export default function Home() {
   return (
-    <main className="max-h-screen h-screen">
-      
-      <section className="h-14">
-        <section className="fixed top-0 left-0 w-screen"> 
-          <Header /> 
-        </section>
-      </section>
+    <main className="h-screen max-h-screen">
+      <div className="fixed top-0 left-0 h-14 w-full z-20">
+        <Header />
+      </div>
+
+      <div className="flex pt-14">
+
+        <div className="w-64 fixed top-14 left-0 h-full">
+          <Sidebar />
+        </div>
+
+        <div className="flex-1 ml-64">
+
+          <div className="flex">
+            <div className="flex-1">
+              <ShadesPreview />
+            </div>
+
+            <div className="w-60 sticky top-14 max-h-full h-[calc(100vh-3.5rem)]">
+              <Banner />
+            </div>
+          </div>
 
 
-      <section className="flex justify-start">
-      
-        <section className="w-64">
-          <section className="fixed top-14 left-0 h-full w-64"> 
-            <Sidebar/>
-          </section>
-        </section>
-
-
-{/* donno why there is an extra 1rem here */}
-        <section className="w-[calc(100vw-17rem)]">
-
-          <section className="flex w-full max-h-full">
-            <section className="w-[calc(100%-15rem)]">
-              <ShadesPreview/>
-            </section>
-
-            <section className="w-60 max-h-full h-[calc(100vh-3.5rem)] sticky top-14">
-              <Banner/>
-            </section>
-          </section>
-
-          <section className="w-full">
-            <UiExamples/>
-          </section>
-
-        </section>
-  
-      </section>
+          <div className="w-full">
+            <UiExamples />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
