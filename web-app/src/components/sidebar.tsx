995bbf3a3c36c4b721fe3701c1ec1bbc32460bd7
zeeenku <enajjachi.zakariaa@gmail.com>
2025-02-11 02:55:05 +0100

    sidebar ui finished

--- web-app/src/components/sidebar.tsx
+++ web-app/src/components/sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
     Card,
     CardContent,
@@ -20,185 +22,151 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Alert, AlertTitle, AlertDescription } from "./ui/alert";
+import { useState } from "react";
 
+
+type Color = {
+    hexVal : string,
+    name : string,
+    id : number,
+    role : string
+}
 
 export default function Sidebar() {
+    const cs : Color[] = [
+        {
+            hexVal : "#84cc16",
+            name : "lime",
+            id : 500,
+            role : "primary"
+        },
+        {
+            hexVal : "#4f46e5",
+            name : "indigo",
+            id : 600,
+            role : "secondary"
+        },
+
+
+        {
+            hexVal : "#f472b6",
+            name : "pink",
+            id : 400,
+            role : "tertiary"
+        },
+
+    ];
+    const [colors, setColors] = useState(cs);
+
+    const removeColor = (el : Color) => {
+
+    }
+
+    const addColor = () => {
+
+    }
+
+    const onChangeColor = () => {
+
+    }
+
+    const onPasteColor = () => {
+
+    }
+
+    const onChangeName = () => {
+        
+    }
+
+    const onChangeId = () => {
+
+    }
+
     return (<aside className="bg-slate-50 h-full  w-[22rem]">
     <Card className="h-full max-h-full p-0 pb-14 overflow-y-auto rounded-none bg-slate-50">
     <CardHeader className="p-4">
         <CardTitle>TailShadescn CSS Shades Generator</CardTitle>
-        <CardDescription>The best shades generator beautiful like tailwind shades generator for any color, with a shadcn theme genertor integrated</CardDescription>
+        <CardDescription>The best shades generator, as beautiful as the Tailwind shades, for any color, with an integrated ShadCN theme generator.</CardDescription>
         <Button className="w-full">hello there</Button>
 
     </CardHeader>
     <CardContent className="p-0">
 
 
-        {/* <div className="mt-5 flex justify-between">
-            <span>Shadcn Theme</span>
-            <Switch></Switch>
-        </div> */}
 
 
 
-
-<div className="border-b border-slate-300 pt-3 pb-4 px-4">
-    <div className="flex justify-between items-center mb-2">
-        <h3>Primary</h3>
-
-        <Button variant="outline" size="icon" className="rounded-full bg-slate-50 shadow-none border-none h-7 w-7 p-1">
-            <XIcon className="h-6 w-6"></XIcon>
-        </Button>
-    </div>
-    <Card >
-            <CardContent className="p-3">
-                <div className="flex justify-between items-center">
-            
-
-                <div className="flex items-start justify-center w-[6.25rem] h-7">
-                        <input type="color" id="color" name="color" className="hidden" />
-                        <label htmlFor="color" className="inline-block aspect-square me-1 my-1 h-5 rounded-full bg-slate-900"></label>
-                        <Input type="text" placeholder="color" className="w-full h-7 text-xs px-1 ms-1
-                        focus:outline-none focus:outline-transparent focus:border-none border-none shadow-none" />
-                    </div>
-
-                    <div className="flex">
-                    <div className="relative w-16 ">
-                    <Input type="text" placeholder="name" className="w-full bg-slate-100 border-none 
-                    shadow-none text-slate-700 text-xs text-center h-7 px-1" />
-                    </div>
-                    <span className="mx-1">-</span>
-
-
-                    <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="outline" className="text-xs border-none shadow-none h-7 w-16 bg-slate-100 text-slate-700" >500</Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>Profile</DropdownMenuItem>
-                        <DropdownMenuItem>Billing</DropdownMenuItem>
-                        <DropdownMenuItem>Team</DropdownMenuItem>
-                        <DropdownMenuItem>Subscription</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-                    </div>
-                </div>
-
-            </CardContent>
-
-        </Card>
-
+{colors.map((el)=>{
+    return (<div key={el.role} className="border-b border-slate-300 p-4 pb-5">
+        <div className="flex justify-between items-center mb-1">
+            <h3 className="capitalize text-sm">{el.role}</h3>
+    
+            <Button onClick={()=>removeColor(el)} variant="outline" size="icon" className="rounded-full bg-slate-50 shadow-none border-none h-6 w-6">
+                <XIcon className="h-3 w-3"></XIcon>
+            </Button>
         </div>
+        <Card >
+                <CardContent className="p-3">
+                    <div className="flex justify-between items-center">
+                
+    
+                    <div className="flex items-center justify-center w-28 h-6">
+                            <input type="color" id="color" name="color" className="w-0 h-0" 
+                            value={el.hexVal} onChange={onChangeColor}
+                            />
+                            <label 
+                            htmlFor="color" 
+                            className="inline-block cursor-pointer aspect-square me-1 my-1 h-5 rounded-full"
+                            style={{ backgroundColor: el.hexVal }}
+                            ></label>
 
-
-
-        <div className="border-b border-slate-300 pt-3 pb-4 px-4">
-    <div className="flex justify-between items-center mb-2">
-        <h3>Primary</h3>
-
-        <Button variant="outline" size="icon" className="rounded-full bg-slate-50 shadow-none border-none h-7 w-7 p-1">
-            <XIcon className="h-6 w-6"></XIcon>
-        </Button>
-    </div>
-    <Card >
-            <CardContent className="p-3">
-                <div className="flex justify-between items-center">
-            
-
-                <div className="flex items-start justify-center w-[6.25rem] h-7">
-                        <input type="color" id="color" name="color" className="hidden" />
-                        <label htmlFor="color" className="inline-block aspect-square me-1 my-1 h-5 rounded-full bg-slate-900"></label>
-                        <Input type="text" placeholder="color" className="w-full h-7 text-xs px-1 ms-1
-                        focus:outline-none focus:outline-transparent focus:border-none border-none shadow-none" />
+                        
+                            <Input type="text" 
+                            value={el.hexVal} onChange={onPasteColor}
+                            placeholder="color" className="w-full h-6 text-xs px-1 ms-1
+                            focus:outline-none focus:outline-transparent focus:border-none border-none shadow-none" />
+                        </div>
+    
+                        <div className="flex">
+                        <div className="relative w-16 ">
+                        <Input type="text" placeholder="name" onChange={onChangeName} className="w-full bg-slate-100 border-none 
+                        shadow-none text-slate-700 font-medium text-[0.8rem] md:text-[0.8rem] text-center h-7 px-1"
+                        value={el.name}
+                        />
+                        </div>
+                        <span className="mx-1">-</span>
+    
+    
+                        <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="outline" className="text-xs border-none shadow-none h-7 w-16 bg-slate-100 text-slate-700" >
+                                {el.id}
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem onClick={onChangeId}>Profile</DropdownMenuItem>
+                            <DropdownMenuItem onClick={onChangeId}>Billing</DropdownMenuItem>
+                            <DropdownMenuItem onClick={onChangeId}>Team</DropdownMenuItem>
+                            <DropdownMenuItem onClick={onChangeId}>Subscription</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                        </div>
                     </div>
-
-                    <div className="flex">
-                    <div className="relative w-16 ">
-                    <Input type="text" placeholder="name" className="w-full bg-slate-100 border-none 
-                    shadow-none text-slate-700 text-xs text-center h-7 px-1" />
-                    </div>
-                    <span className="mx-1">-</span>
-
-
-                    <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="outline" className="text-xs border-none shadow-none h-7 w-16 bg-slate-100 text-slate-700" >500</Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>Profile</DropdownMenuItem>
-                        <DropdownMenuItem>Billing</DropdownMenuItem>
-                        <DropdownMenuItem>Team</DropdownMenuItem>
-                        <DropdownMenuItem>Subscription</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-                    </div>
-                </div>
-
-            </CardContent>
-
-        </Card>
-
-        </div>
+    
+                </CardContent>
+    
+            </Card>
+    
+            </div>);
+})}
 
 
 
-
-        <div className="border-b border-slate-300 pt-3 pb-4 px-4">
-    <div className="flex justify-between items-center mb-2">
-        <h3>Primary</h3>
-
-        <Button variant="outline" size="icon" className="rounded-full bg-slate-50 shadow-none border-none h-7 w-7 p-1">
-            <XIcon className="h-6 w-6"></XIcon>
-        </Button>
-    </div>
-    <Card >
-            <CardContent className="p-3">
-                <div className="flex justify-between items-center">
-            
-
-                <div className="flex items-start justify-center w-[6.25rem] h-7">
-                        <input type="color" id="color" name="color" className="hidden" />
-                        <label htmlFor="color" className="inline-block aspect-square me-1 my-1 h-5 rounded-full bg-slate-900"></label>
-                        <Input type="text" placeholder="color" className="w-full h-7 text-xs px-1 ms-1
-                        focus:outline-none focus:outline-transparent focus:border-none border-none shadow-none" />
-                    </div>
-
-                    <div className="flex">
-                    <div className="relative w-16 ">
-                    <Input type="text" placeholder="name" className="w-full bg-slate-100 border-none 
-                    shadow-none text-slate-700 text-xs text-center h-7 px-1" />
-                    </div>
-                    <span className="mx-1">-</span>
-
-
-                    <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button variant="outline" className="text-xs border-none shadow-none h-7 w-16 bg-slate-100 text-slate-700" >500</Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>Profile</DropdownMenuItem>
-                        <DropdownMenuItem>Billing</DropdownMenuItem>
-                        <DropdownMenuItem>Team</DropdownMenuItem>
-                        <DropdownMenuItem>Subscription</DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-                    </div>
-                </div>
-
-            </CardContent>
-
-        </Card>
-
-        </div>
 
             <div className="p-4">
-                <Button className="w-full my-2">Add new color</Button>
+                <Button onClick={addColor} className="w-full my-2">Add new color</Button>
             </div>
     </CardContent>
     <CardFooter className="p-4">
